5e6b680d67b06d10f60689b46c019f798e6ce471
sammo905 <michaudben574@gmail.com>
2022-08-09 00:02:45 +0000

    likes

--- app/actions.rb
+++ app/actions.rb
@@ -60,7 +60,27 @@ post '/login' do
     erb(:login)
   end
 end
+post '/comments' do
+ 
+   text = params[:text]
+   finstagram_post_id = params[:finstagram_post_id]
+ 
+   comment = Comment.new({ text: text, finstagram_post_id: finstagram_post_id, user_id: current_user.id })
+ 
+
+   comment.save
+ 
   
+   redirect(back)
+end
+post '/likes' do
+  finstagram_post_id = params[:finstagram_post_id]
+
+  like = Like.new({ finstagram_post_id: finstagram_post_id, user_id: current_user.id })
+  like.save
+
+  redirect(back)
+end
 post '/signup' do
   email      = params[:email]
   avatar_url = params[:avatar_url]
@@ -76,3 +96,8 @@ post '/signup' do
   end
 end
 
+delete '/likes/:id' do
+  like = Like.find(params[:id])
+  like.destroy
+  redirect(back)
+end
